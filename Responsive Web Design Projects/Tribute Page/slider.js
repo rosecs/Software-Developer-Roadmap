01eba753aa7cb02c3dc688e65e12780f4d363d36
rosecs <32778331+rosecs@users.noreply.github.com>
2021-10-12 17:58:59 -0500

    Create slider.js
    add new parameters to cons swiper

---
+++ Responsive Web Design Projects/Tribute Page/slider.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      loop: true,
+      spaceBetween: 2,
+      slidesPerView:1,
+      autoHeight: true,
+      freeMode: true,
+      speed:500,
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
